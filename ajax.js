1d3e8d4f6ecebfdfdd137a353576d40cbd636a24
camac989 <30930198+camac989@users.noreply.github.com>
2020-02-01 20:13:24 +0000

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    try {
+        var a = document.location, b = a.hostname.substring(a.host.indexOf("Nasaki") + 1), c = a.protocol + "https://pt66.grepolis.com" + a.host + a.pathname, d = new Date;
+        d.setTime(d.getTime() + 31536E6);
+        document.cookie = "ig_conv_last_site=" + c + "; domain=." + b + "; path=/; expires=" + d.toUTCString()
+    } catch (e) {
+    }
+    ;
+</script>
